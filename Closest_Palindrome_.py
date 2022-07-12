4a58694ed59762f746ee0a4b34249cd29e4b4918
Trinadh7 <97424596+Trinadh7@users.noreply.github.com>
2022-07-12 11:57:23 +0530

    def pal(n):     t=n     a=0     while(n):         d=n%10         a=a*10+d         n=n//10     if(a==t):         return 1     else:         return 0 n=int(input()) for i in range(n+1,1000000):     if pal(i):         a=i         break for i in range(n-1,0,-1):     if pal(i):         b=i         break if abs(b-n)==abs(a-n):     print(b,a) elif abs(b-n)<abs(a-n):     print(b) else:     print(a)

--- Closest_Palindrome_.py
+++ Closest_Palindrome_.py
@@ -1,26 +1,28 @@
 def pal(n):
-   t=n
-   a=0 
-   while(n):
-       d=n%10
-       a=a*10+d
-       n=n//10
-   if  (a==t):
-         return 1
-   else:
+    t=n
+    a=0
+    while(n):
+        d=n%10
+        a=a*10+d
+        n=n//10
+    if(a==t):
+        return 1
+    else:
         return 0
-n= int(input())
-for i in range(n+1,100000):
+n=int(input())
+for i in range(n+1,1000000):
     if pal(i):
         a=i
         break
 for i in range(n-1,0,-1):
     if pal(i):
-        a1=i
+        b=i
         break
-if abs(a1-n)==abs(a-n):
-    print(a1,a)
-elif abs(a1-n)<abs(a-n):
-    print(a1)    
+if abs(b-n)==abs(a-n):
+    print(b,a)
+elif abs(b-n)<abs(a-n):
+    print(b)
 else:
     print(a)
+    
+        
