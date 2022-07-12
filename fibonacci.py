b37d644a17a98c56f64f09c18516b03d6d7fad4c
Trinadh7 <97424596+Trinadh7@users.noreply.github.com>
2022-07-12 21:19:53 +0530

    n=int(input()) a=0 b=1 c=0 for i in range(0,n):     print(a,end=" ")     c=a+b     a=b     b=c

--- fibonacci.py
+++ fibonacci.py
@@ -1,9 +1,10 @@
-a=int(input())
-fa=0
-fb=1
-n=0
-for i in range(a):
-    print(fa,end=" ")
-    n=fa+fb
-    fa=fb
-    fb=n
+n=int(input())
+a=0
+b=1
+c=0
+for i in range(0,n):
+    print(a,end=" ")
+    c=a+b
+    a=b
+    b=c
+    
